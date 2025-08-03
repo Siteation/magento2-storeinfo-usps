b64b34b9cce2bc51c56e0ef3f521fcdbb6808673
allrude <allrude@gmail.com>
2025-08-03 17:08:00 +0200

    ADD: csp to sorter
    Co-authored-by: GrimLink <sean.grimlink@gmail.com>
    Co-authored-by: allrude <allrude@gmail.com>

--- Block/Adminhtml/Form/Field/Usps.php
+++ Block/Adminhtml/Form/Field/Usps.php
@@ -8,14 +8,25 @@
 
 namespace Siteation\StoreInfoUsps\Block\Adminhtml\Form\Field;
 
+use Magento\Backend\Block\Template\Context;
 use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
 use Magento\Framework\Data\Form\Element\AbstractElement;
 use Magento\Framework\DataObject;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
 use Siteation\StoreInfoUsps\Block\Adminhtml\Form\Field\IconOptions;
 
 class Usps extends AbstractFieldArray
 {
     private $iconRenderer;
+
+    public function __construct(
+        Context $context,
+        SecureHtmlRenderer $secureRenderer,
+        array $data = []
+    ) {
+        $this->secureRenderer = $secureRenderer;
+        parent::__construct($context, $data);
+    }
 
     protected function _prepareToRender()
     {
@@ -62,21 +73,13 @@ class Usps extends AbstractFieldArray
         $id = $element['html_id'];
         $html = parent::_getElementHtml($element);
 
-        $script = "<script>
-            document.addEventListener('DOMContentLoaded', function(event) {
-                require(['jquery', 'Magento_Theme/js/sortable'], function ($) {
-                    $('#" . $id . "').sortable({
-                        containment: 'parent',
-                        items: 'tbody tr',
-                        tolerance: 'pointer'
-                    });
-
-                    $('#" . $id . " tbody tr td:last-child').prepend('<span class=\"draggable-handle\" style=\"display: inline-flex; justify-content: center; align-items: center; margin-block: 0.5rem; margin-inline-end: 2rem; vertical-align: baseline;\"></span>');
-                });
+        $scriptString = <<<script
+            require(['jquery', 'Magento_Theme/js/sortable'], function ($) {
+                $('#$id').sortable({ containment: 'parent', items: 'tbody tr', tolerance: 'pointer' });
             });
-        </script>";
+        script;
 
-        $html .= $script;
+        $html .= $this->secureRenderer->renderTag('script', [], $scriptString, false);
 
         return $html;
     }
